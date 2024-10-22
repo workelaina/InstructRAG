c7864762a5a49b037af908f06cf0ed93b7038815
userElaina <userelaina@pm.me>
2024-10-22 08:22:38 +0000

    :art: fmt *.sh

--- generate_rationale.sh
+++ generate_rationale.sh
@@ -4,5 +4,5 @@ CUDA_VISIBLE_DEVICES=0 python src/inference.py \
   --dataset_name $DATASET \
   --model_name_or_path meta-llama/Meta-Llama-3-8B-Instruct \
   --n_docs 5 \
-  --output_dir dataset/${DATASET}\
+  --output_dir dataset/${DATASET} \
   --do_rationale_generation \
